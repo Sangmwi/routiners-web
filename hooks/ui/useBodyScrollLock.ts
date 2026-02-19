8df8d3cc269ea941a76f5e18ada2243650589ff2
Sangmwi Han <gkstkdgnl12@naver.com>
2026-02-20 00:24:46 +0900

    모달 스크롤문제 해결

--- hooks/ui/useBodyScrollLock.ts
+++ hooks/ui/useBodyScrollLock.ts
@@ -5,14 +5,22 @@ import { useEffect } from 'react';
 /**
  * Body 스크롤 잠금 훅
  * 모달/드로어 열릴 때 배경 스크롤 방지
+ * Reference counting으로 중첩 모달 지원
  */
+let lockCount = 0;
+
 export function useBodyScrollLock(isLocked: boolean) {
   useEffect(() => {
     if (isLocked) {
-      const originalOverflow = document.body.style.overflow;
-      document.body.style.overflow = 'hidden';
+      lockCount++;
+      if (lockCount === 1) {
+        document.body.style.overflow = 'hidden';
+      }
       return () => {
-        document.body.style.overflow = originalOverflow;
+        lockCount--;
+        if (lockCount === 0) {
+          document.body.style.overflow = '';
+        }
       };
     }
   }, [isLocked]);
